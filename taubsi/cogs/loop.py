371853c499f7c0b2c43885887880d4477b00e110
ReuschelCGN <82573872+ReuschelCGN@users.noreply.github.com>
2022-10-18 13:29:50 +0200

    Update loop.py

--- taubsi/cogs/loop.py
+++ taubsi/cogs/loop.py
@@ -2,6 +2,9 @@ from taubsi.utils.logging import log
 from taubsi.taubsi_objects import tb
 from discord.ext import tasks, commands
 
+import json
+import requests
+
 class Loop(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -12,6 +15,19 @@ class Loop(commands.Cog):
     async def big_loop(self):
         tb.reload_pogodata()
         log.info("Reloaded PogoData")
+
+        try:
+            tb.localenames_en = requests.get("https://raw.githubusercontent.com/WatWowMap/pogo-translations/master/static/locales/en.json").json()
+        except Exception:
+            pass
+        if not tb.localenames_en:
+            log.info(f"Error while requesting english locals from pogo-translations.")
+        try:
+            tb.localenames_de = requests.get("https://raw.githubusercontent.com/WatWowMap/pogo-translations/master/static/locales/de.json").json()
+        except Exception:
+            pass
+        if not tb.localenames_de:
+            log.info(f"Error while requesting german locals from pogo-translations.")
 
 def setup(bot):
     bot.add_cog(Loop(bot))
