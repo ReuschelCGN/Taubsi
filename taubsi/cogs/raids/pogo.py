1a2f24185f5c8768ef42a8f89a0e977c706ece1d
ReuschelCGN <82573872+ReuschelCGN@users.noreply.github.com>
2023-02-22 13:31:06 +0100

    Update pogo.py

--- taubsi/cogs/raids/pogo.py
+++ taubsi/cogs/raids/pogo.py
@@ -134,7 +134,9 @@ class BaseRaid:
                 self.name = f"{tb.translate('ultra_wormhole_opend')} 🌀"
             elif (self.level == 9):
                 self.name = f"{tb.translate('elite')} {tb.translate('Egg')} ⚡🥚"
-            elif ((self.level < 6) or (self.level > 9)):
+            elif (self.level == 10:
+                self.name = f"{tb.translate('primal')} {tb.translate('Egg')} ⚡🥚"
+            elif ((self.level < 6) or (self.level > 10):
                 self.name = f"Level {self.level} {tb.translate('Egg')} 🥚"
 
         self.egg_url = (
